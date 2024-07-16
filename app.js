b13cab456db2e0b6283019274fc1aaf5b95e55ce
Omadbek571 <omadbektomsk08@gmail.com>
2024-07-16 15:44:28 +0500

    .

---
+++ app.js
@@ -0,0 +1,135 @@
+// 1-savol
+
+// let num = 5;
+// let res = Math.floor(num / 2) 
+// let natija = res % 2;
+
+// console.log("butun qisim " + res);
+// console.log("qoldiq qisim " + natija);
+
+// 2- savol
+
+// let num1 = 5;
+// let num2 = 4;
+// let sum;
+
+// if (num1 > num2) {
+//      sum = num1 * num2
+// }else if(num1 < num2){
+//      sum = num1 + num2
+// }else{
+//     sum = num1 - num2
+// }
+
+// console.log(sum);
+
+
+// let num1 = +prompt("Iltimos son kiriting");
+// let sum = 1;
+
+// for (let i = 1; i <= num1; i++) {
+//     sum *= i
+// }
+
+// console.log(sum);
+
+
+// 4-savol
+
+// let num1 = 4;
+// let num2 = 4;
+// let num3 = 4;
+
+// let sum1 = num1 * num1
+// let sum2 = num2 * num2
+// let sum3 = num3 * num3
+
+// let res =  sum1 + sum2 + sum3
+
+// console.log(res);
+
+
+// 5-savol
+
+// let a = +prompt("Iltimos son kiriting");
+// let b = +prompt("Iltimos son kiriting");
+
+// let resA = a ** b
+// let resB = a ** b
+
+
+// console.log(resA);
+// console.log(resB);
+
+
+
+
+
+// 6-savol
+
+
+// let num1 = 6.2;
+
+// let res = Math.floor(num1 % 4)
+
+// console.log(res);
+
+
+
+// 7-savol
+
+// let num1 = +prompt("1 Iltimos son kiriting");
+// let num2 = +prompt("2 Iltimos son kiriting");
+
+// let res1 = num1 ** num2;
+// let res2 = num2 ** num2;
+
+// console.log(res1);
+// console.log(res2);
+
+
+// QOSHIMCHA MISOLAR 
+
+// 1-savol
+
+// let L = +prompt("Iltimos santimetir kiriting");
+// let res = L / 100;
+
+// console.log(L + " yozgan santimetiriz metirda shuncha boladi " + res);
+
+
+
+// 2-savol
+
+
+// let m = +prompt("Iltimos kg kiriting");
+// let res = Math.trunc(m / 1000)
+
+// console.log("Yozgan kg miz tonnada shuncha boladi  "+res);
+
+
+// 3-savol 
+
+// let bayt = 5451184;
+// let res = Math.trunc(bayt / 1024);
+
+// console.log(res);
+
+
+// 4-savol
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
